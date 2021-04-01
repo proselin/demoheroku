27678802fddf1f7f21b3baea8f7b0d0306893627
proselin <72499558+proselin@users.noreply.github.com>
2021-04-01 16:59:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-	include('conn.php');
+$conn = pg_connect("host=ec2-54-145-249-177.compute-1.amazonaws.com dbname=d7j76pvrbbruf1 user=pgctruknafgbyl password=761b07a6272985b1dbd1157323d816331303f4e3a54b3f76b23bb4f460dd7351");
+if($conn){echo " connect";}
  if(isset($_POST['login'])){
 	$username = $_POST['username'];
  	$password = $_POST['password'];
